e2ffbec3b01ac47ffc3bb68a54fa7d6cda4d2586
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-08-31 17:59:27 +0200

    api.js with fetch and api key added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { debounce } from 'lodash';
+import { fetchImages } from 'api/api';
 
 import Searchbar from './Searchbar/index';
 import ImageGallery from './ImageGallery/index';
 import Button from './Button/index';
 import Loader from './Loader/index';
 import Modal from './Modal/index';
-
-const API_KEY = '36836755-9f43607b903fa703cdff42e50';
-const API_URL = 'https://pixabay.com/api/';
 
 class App extends Component {
   state = {
@@ -39,14 +36,7 @@ class App extends Component {
   fetchImages = debounce(async (query = this.state.inputValue) => {
     this.setState({ loading: true });
     try {
-      const response = await axios.get(
-        `${API_URL}?q=${query}&page=${this.state.currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-      );
-
-      const newImages = response.data.hits.filter(
-        image =>
-          !this.state.images.some(stateImage => stateImage.id === image.id)
-      );
+      const newImages = await fetchImages(query, this.state.currentPage); // Zmieniono
 
       this.setState(prevState => ({
         images: [...prevState.images, ...newImages],
