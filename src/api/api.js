04183ad4c2eb9bf65a200df4f6ae6e8eea2da80d
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-09-01 11:47:47 +0200

    constans in api folder added

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { PER_PAGE } from './constans';
 
 const API_KEY = '36836755-9f43607b903fa703cdff42e50';
 const API_URL = 'https://pixabay.com/api/';
@@ -6,7 +7,7 @@ const API_URL = 'https://pixabay.com/api/';
 export const fetchImages = async (query, currentPage) => {
   try {
     const response = await axios.get(
-      `${API_URL}?q=${query}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      `${API_URL}?q=${query}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}`
     );
     return response.data.hits;
   } catch (error) {
